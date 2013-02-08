573d449def0b8c9fe1490f78c4ff0f7b57003466
Stefan Hedman <schteppe@gmail.com>
2013-02-08 11:24:02 +0100

    renamed springs example

--- spring2d.js
+++ spring2d.js
@@ -1,6 +1,6 @@
 /**
  * spring2d.js
- * Physics library for simulating springs and particles
+ * Physics library for simulating springs and bodies
  * @author Stefan Hedman <schteppe@gmail.com>
  */
 var Spring2D = {};
@@ -10,57 +10,57 @@ var Spring2D = {};
 
     S.World = function(){
         this.springs = [];
-        this.particles = [];
+        this.bodies = [];
         this.gravity = new S.Vec2();
     };
     S.World.prototype.step = function(dt){
         var Nsprings = this.springs.length,
             springs = this.springs,
-            particles = this.particles
+            bodies = this.bodies
             g = this.gravity,
             gx = g.x,
             gy = g.y,
-            Nparticles = this.particles.length;
+            Nbodies = this.bodies.length;
 
         // Reset forces, add gravity
-        for(var i=0; i!==Nparticles; i++){
-            particles[i].force.set(gx,gy);
+        for(var i=0; i!==Nbodies; i++){
+            bodies[i].force.set(gx,gy);
         }
 
-        // Calculate all new forces
+        // Calculate all new spring forces
         for(var i=0; i!==Nsprings; i++){
             var s = springs[i];
             var k = s.stiffness;
             var d = s.damping;
             var l = s.restLength;
-            var particleA = s.particleA;
-            var particleB = s.particleB;
+            var bodyA = s.bodyA;
+            var bodyB = s.bodyB;
 
-            var rx = particleA.position.x - particleB.position.x;
-            var ry = particleA.position.y - particleB.position.y;
-            var ux = particleA.velocity.x - particleB.velocity.x;
-            var uy = particleA.velocity.y - particleB.velocity.y;
+            var rx = bodyA.position.x - bodyB.position.x;
+            var ry = bodyA.position.y - bodyB.position.y;
+            var ux = bodyA.velocity.x - bodyB.velocity.x;
+            var uy = bodyA.velocity.y - bodyB.velocity.y;
             var rlen = sqrt(rx*rx + ry*ry);
             var fx = -( k*(rlen-l) + d*ux*rx/rlen ) * rx/rlen;
             var fy = -( k*(rlen-l) + d*uy*ry/rlen ) * ry/rlen;
 
-            particleA.force.x += fx;
-            particleA.force.y += fy;
-            particleB.force.x -= fx;
-            particleB.force.y -= fy;
+            bodyA.force.x += fx;
+            bodyA.force.y += fy;
+            bodyB.force.x -= fx;
+            bodyB.force.y -= fy;
         }
 
         // Step
-        for(var i=0; i!==Nparticles; i++){
-            var particle = particles[i];
-            if(particle.mass>0){
-                var minv = 1.0 / particle.mass,
-                    f = particle.force,
-                    pos = particle.position,
-                    velo = particle.velocity;
+        for(var i=0; i!==Nbodies; i++){
+            var body = bodies[i];
+            if(body.mass>0){
+                var minv = 1.0 / body.mass,
+                    f = body.force,
+                    pos = body.position,
+                    velo = body.velocity;
                 velo.x += f.x * dt * minv;
                 velo.y += f.y * dt * minv;
-                pos.x += velo.x * dt; // Particle: using new velocity to step
+                pos.x += velo.x * dt; // body: using new velocity to step
                 pos.y += velo.y * dt;
             }
         }
@@ -73,28 +73,40 @@ var Spring2D = {};
         if(idx===-1)
             this.springs.splice(idx,1);
     };
-    S.World.prototype.addParticle = function(particle){
-        this.particles.push(particle);
+    S.World.prototype.addBody = function(body){
+        this.bodies.push(body);
     };
-    S.World.prototype.removeParticle = function(particle){
-        var idx = this.particles.indexOf(particle);
+    S.World.prototype.removeBody = function(body){
+        var idx = this.bodies.indexOf(body);
         if(idx===-1)
-            this.particles.splice(idx,1);
+            this.bodies.splice(idx,1);
     };
 
-    S.Spring = function(particleA,particleB){
+    S.Spring = function(bodyA,bodyB){
         this.restLength = 1;
         this.stiffness = 100;
         this.damping = 1;
-        this.particleA = particleA;
-        this.particleB = particleB;
+        this.bodyA = bodyA;
+        this.bodyB = bodyB;
     };
 
-    S.Particle = function(){
+    S.Body = function(shape){
         this.mass = 1;
         this.position = new S.Vec2(0,0);
         this.velocity = new S.Vec2(0,0);
         this.force = new S.Vec2(0,0);
+        this.shape = shape;
+    };
+
+    S.Shape = function(){};
+
+    S.Particle = function(){
+        S.Shape.apply(this);
+    };
+
+    S.Circle = function(){
+        S.Shape.apply(this);
+        this.radius = 1;
     };
 
     S.Vec2 = function(x,y){
