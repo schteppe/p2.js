72b40a2b515fcea36d7b1475e41b7df970c1e5c9
Stefan Hedman <schteppe@gmail.com>
2013-02-08 13:09:34 +0100

    added preliminart contactequation etc

--- spring2d.js
+++ spring2d.js
@@ -11,6 +11,7 @@ var Spring2D = {};
     S.World = function(){
         this.springs = [];
         this.bodies = [];
+        this.solver = new S.Solver();
         this.contacts = [];
         this.collidingBodies = [];
         this.gravity = new S.Vec2();
@@ -23,6 +24,7 @@ var Spring2D = {};
             g = this.gravity,
             gx = g.x,
             gy = g.y,
+            solver = this.solver,
             Nbodies = this.bodies.length;
 
         // Reset forces, add gravity
@@ -80,8 +82,32 @@ var Spring2D = {};
             }
         }
 
-        // Generate contacts
-        this.contacts = []; // todo
+        // Generate contacts / do nearphase
+        function nearphaseCircleCircle(c1,c2,result){
+            var c = new S.ContactEquation(c1,c2);
+            c2.position.vsub(c1.position,c.ni);
+            c.ni.normalize();
+            c.ni.mult(c1.shape.radius, c.ri);
+            c.ni.mult(c2.shape.radius, c.rj);
+            result.push(c);
+        }
+        function nearphaseCircleParticle(c,p,result){
+            // todo
+        }
+        var contacts = this.contacts = []; // todo
+        for(var i=0, Nresults=result.length; i!==Nresults; i+=2){
+            var bi = result[i];
+            var bj = result[i+1];
+            if(si instanceof S.Circle){
+                     if(sj instanceof S.Circle)   nearphaseCircleCircle(bi,bj,contacts);
+                else if(sj instanceof S.Particle) nearphaseCircleParticle(bi,bj,contacts);
+            } else if(si instanceof S.Particle){
+                     if(sj instanceof S.Circle)   nearphaseCircleParticle(bj,bi,contacts);
+            }
+        }
+
+        // Solver
+        solver.solve();
 
         // Step
         for(var i=0; i!==Nbodies; i++){
@@ -164,7 +190,7 @@ var Spring2D = {};
         this.y=y;
     };
     S.Vec2.prototype.vadd = function(v,target){
-        target = target || new S.Vec3();
+        target = target || new S.Vec2();
         target.x += v.x;
         target.y += v.y;
     };
@@ -203,6 +229,157 @@ var Spring2D = {};
     };
     S.Solver.prototype.removeAllEquations = function(){
         this.equations = [];
+    };
+
+    S.Equation = function(bi,bj,minForce,maxForce){
+      this.id = -1;
+      this.minForce = typeof(minForce)=="undefined" ? -1e6 : minForce;
+      this.maxForce = typeof(maxForce)=="undefined" ? 1e6 : maxForce;
+      this.bi = bi;
+      this.bj = bj;
+    };
+    S.Equation.prototype.constructor = S.Equation;
+
+
+    S.ContactEquation = function(bi,bj){
+        S.Equation.call(this,bi,bj,0,1e6);
+        this.penetration = 0.0;
+        this.ri = new S.Vec2();
+        this.penetrationVec = new S.Vec2();
+        this.rj = new S.Vec2();
+        this.ni = new S.Vec2();
+        this.rixn = new S.Vec2();
+        this.rjxn = new S.Vec2();
+        this.rixw = new S.Vec2();
+        this.rjxw = new S.Vec2();
+
+        this.invIi = new S.Mat3();
+        this.invIj = new S.Mat3();
+
+        this.relVel = new S.Vec2();
+        this.relForce = new S.Vec2();
+    };
+    S.ContactEquation.prototype = new S.Equation();
+    S.ContactEquation.prototype.constructor = S.ContactEquation;
+    S.ContactEquation.prototype.computeB = function(a,b,h){
+        var bi = this.bi;
+        var bj = this.bj;
+        var ri = this.ri;
+        var rj = this.rj;
+        var rixn = this.rixn;
+        var rjxn = this.rjxn;
+
+        var vi = bi.velocity;
+        var wi = bi.angularVelocity ? bi.angularVelocity : new S.Vec2();
+        var fi = bi.force;
+        var taui = bi.tau ? bi.tau : new S.Vec2();
+
+        var vj = bj.velocity;
+        var wj = bj.angularVelocity ? bj.angularVelocity : new S.Vec2();
+        var fj = bj.force;
+        var tauj = bj.tau ? bj.tau : new S.Vec2();
+
+        var relVel = this.relVel;
+        var relForce = this.relForce;
+        var penetrationVec = this.penetrationVec;
+        var invMassi = bi.invMass;
+        var invMassj = bj.invMass;
+
+        var invIi = this.invIi;
+        var invIj = this.invIj;
+
+        if(bi.invInertia) invIi.setTrace(bi.invInertia);
+        else              invIi.identity(); // ok?
+        if(bj.invInertia) invIj.setTrace(bj.invInertia);
+        else              invIj.identity(); // ok?
+
+        var n = this.ni;
+
+        // Caluclate cross products
+        ri.cross(n,rixn);
+        rj.cross(n,rjxn);
+
+        // Calculate q = xj+rj -(xi+ri) i.e. the penetration vector
+        var penetrationVec = this.penetrationVec;
+        penetrationVec.set(0,0,0);
+        penetrationVec.vadd(bj.position,penetrationVec);
+        penetrationVec.vadd(rj,penetrationVec);
+        penetrationVec.vsub(bi.position,penetrationVec);
+        penetrationVec.vsub(ri,penetrationVec);
+
+        var Gq = n.dot(penetrationVec);//-Math.abs(this.penetration);
+
+        // Compute iteration
+        var GW = vj.dot(n) - vi.dot(n) + wj.dot(rjxn) - wi.dot(rixn);
+        var GiMf = fj.dot(n)*invMassj - fi.dot(n)*invMassi + rjxn.dot(invIj.vmult(tauj)) - rixn.dot(invIi.vmult(taui)) ;
+
+        var B = - Gq * a - GW * b - h*GiMf;
+
+        return B;
+    };
+    // Compute C = GMG+eps in the SPOOK equation
+    S.ContactEquation.prototype.computeC = function(eps){
+        var bi = this.bi;
+        var bj = this.bj;
+        var rixn = this.rixn;
+        var rjxn = this.rjxn;
+        var invMassi = bi.invMass;
+        var invMassj = bj.invMass;
+
+        var C = invMassi + invMassj + eps;
+
+        var invIi = this.invIi;
+        var invIj = this.invIj;
+
+        if(bi.invInertia) invIi.setTrace(bi.invInertia);
+        else              invIi.identity(); // ok?
+        if(bj.invInertia) invIj.setTrace(bj.invInertia);
+        else              invIj.identity(); // ok?
+
+        // Compute rxn * I * rxn for each body
+        C += invIi.vmult(rixn).dot(rixn);
+        C += invIj.vmult(rjxn).dot(rjxn);
+
+        return C;
+    };
+    var computeGWlambda_ulambda = new S.Vec2();
+    S.ContactEquation.prototype.computeGWlambda = function(){
+        var bi = this.bi;
+        var bj = this.bj;
+        var ulambda = computeGWlambda_ulambda;
+
+        var GWlambda = 0.0;
+        bj.vlambda.vsub(bi.vlambda, ulambda);
+        GWlambda += ulambda.dot(this.ni);
+
+        // Angular
+        if(bi.wlambda) GWlambda -= bi.wlambda.dot(this.rixn);
+        if(bj.wlambda) GWlambda += bj.wlambda.dot(this.rjxn);
+        
+        return GWlambda;
+    };
+    S.ContactEquation.prototype.addToWlambda = function(deltalambda){
+        var bi = this.bi;
+        var bj = this.bj;
+        var rixn = this.rixn;
+        var rjxn = this.rjxn;
+        var invMassi = bi.invMass;
+        var invMassj = bj.invMass;
+        var n = this.ni;
+
+        // Add to linear velocity
+        bi.vlambda.vsub(n.mult(invMassi * deltalambda),bi.vlambda);
+        bj.vlambda.vadd(n.mult(invMassj * deltalambda),bj.vlambda);
+
+        // Add to angular velocity
+        if(bi.wlambda){
+            var I = this.invIi;
+            bi.wlambda.vsub(I.vmult(rixn).mult(deltalambda),bi.wlambda);
+        }
+        if(bj.wlambda){
+            var I = this.invIj;
+            bj.wlambda.vadd(I.vmult(rjxn).mult(deltalambda),bj.wlambda);
+        }
     };
 
     S.GSSolver = function(){
