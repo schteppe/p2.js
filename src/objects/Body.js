3934c9ad49e57ce1d8f35631208a73e2107d019c
Stefan Hedman <schteppe@gmail.com>
2013-05-15 19:23:44 +0300

    Document Body.js

--- src/objects/Body.js
+++ src/objects/Body.js
@@ -1,32 +1,59 @@
+/**
+ * A spring, connecting two bodies.
+ * 
+ * Options
+ * - restLength: A number > 0. Default is 1.
+ * - stiffness: A number >= 0 . Default is 100.
+ * - damping: A number >= 0.
+ * 
+ * @class
+ * @param {p2.Body} bodyA
+ * @param {p2.Body} bodyB
+ * @param {Object} options
+ */
+p2.Spring = function(bodyA,bodyB,options){
+    options = options || {};
+    this.restLength = options.restLength || 1;
+    this.stiffness = options.stiffness || 100;
+    this.damping = options.damping || 1;
+    this.bodyA = bodyA;
+    this.bodyB = bodyB;
+};
 
-    p2.Spring = function(bodyA,bodyB,options){
-        options = options || {};
-        this.restLength = options.restLength || 1;
-        this.stiffness = options.stiffness || 100;
-        this.damping = options.damping || 1;
-        this.bodyA = bodyA;
-        this.bodyB = bodyB;
-    };
+/**
+ * A physics body.
+ * 
+ * Options:
+ * - mass: A number >= 0. If zero, the body becomes static. Default is 0.
+ * - position (vec2)
+ * - velocity (vec2)
+ * - angle (vec2)
+ * - angularVelocity (vec2)
+ * - force (vec2)
+ * - angularForce (vec2)
+ * 
+ * @class
+ * @param {Object} options
+ */
+p2.Body = function(options){
+    options = options || {};
 
-    p2.Body = function(options){
-        options = options || {};
+    this.shape = options.shape;
 
-        this.shape = options.shape;
+    this.mass = options.mass || 0;
+    this.invMass = this.mass > 0 ? 1 / this.mass : 0;
+    this.inertia = options.inertia || this.mass; // todo
+    this.invInertia = this.invMass; // todo
 
-        this.mass = options.mass || 0;
-        this.invMass = this.mass > 0 ? 1 / this.mass : 0;
-        this.inertia = options.inertia || this.mass; // todo
-        this.invInertia = this.invMass; // todo
+    this.position = options.position || V.create();
+    this.velocity = options.velocity || V.create();
 
-        this.position = options.position || V.create();
-        this.velocity = options.velocity || V.create();
+    this.vlambda = V.create();
+    this.wlambda = 0;
 
-        this.vlambda = V.create();
-        this.wlambda = 0;
+    this.angle = options.angle || 0;
+    this.angularVelocity = options.angularVelocity || 0;
 
-        this.angle = options.angle || 0;
-        this.angularVelocity = options.angularVelocity || 0;
-
-        this.force = options.force || V.create();
-        this.angularForce = options.angularForce || 0;
-    };
+    this.force = options.force || V.create();
+    this.angularForce = options.angularForce || 0;
+};
