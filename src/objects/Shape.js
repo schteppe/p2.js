ecea25af58344434a6947daf42f086bd0397d03d
Stefan Hedman <schteppe@gmail.com>
2013-05-16 17:33:35 +0200

    Shapes doc

--- src/objects/Shape.js
+++ src/objects/Shape.js
@@ -1,15 +1,33 @@
-
+    /**
+     * Base class for shapes.
+     * @class
+     */
     p2.Shape = function(){};
 
+    /**
+     * Particle shape class.
+     * @class
+     * @extends p2.Shape
+     */
     p2.Particle = function(){
         p2.Shape.apply(this);
     };
 
+    /**
+     * Circle shape class.
+     * @class
+     * @extends p2.Shape
+     */
     p2.Circle = function(radius){
         p2.Shape.apply(this);
         this.radius = radius || 1;
     };
 
+    /**
+     * Plane shape class.
+     * @class
+     * @extends p2.Shape
+     */
     p2.Plane = function(){
         p2.Shape.apply(this);
     };
