b9415be15367ea4af5a53bc0cb6a0380bcf81fd1
Stefan Hedman <schteppe@gmail.com>
2013-05-16 14:16:46 +0300

    Equation doc

--- src/constraints/Equation.js
+++ src/constraints/Equation.js
@@ -1,4 +1,11 @@
-
+    /**
+     * Base class for constraint equations.
+     * @class
+     * @param {p2.Body} bi First body participating in the equation
+     * @param {p2.Body} bj Second body participating in the equation
+     * @param {number} minForce Minimum force to apply. Default: -1e6
+     * @param {number} maxForce Maximum force to apply. Default: 1e6
+     */
    p2.Equation = function(bi,bj,minForce,maxForce){
       this.id = -1;
       this.minForce = typeof(minForce)=="undefined" ? -1e6 : minForce;
