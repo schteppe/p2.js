b4b9be2264742e272d83f032bc31bb43aa897786
Stefan Hedman <schteppe@gmail.com>
2013-09-09 13:00:10 +0200

    added docs

---
+++ docs/api.js
@@ -0,0 +1,27 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Body",
+        "Broadphase",
+        "Circle",
+        "Constraint",
+        "ContactEquation",
+        "DistanceConstraint",
+        "Equation",
+        "GSSolver",
+        "GridBroadphase",
+        "IslandSolver",
+        "NaiveBroadphase",
+        "Particle",
+        "Plane",
+        "Shape",
+        "Solver",
+        "Spring",
+        "World",
+        "vec2",
+        null
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
