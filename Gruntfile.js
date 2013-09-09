142f7869cd15644746da12a78f0f924edce85e39
Stefan Hedman <schteppe@gmail.com>
2013-09-09 09:48:44 +0200

    adding license text to built files

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,10 +25,20 @@ module.exports = function(grunt) {
 
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-browserify');
-    grunt.registerTask('default', ['browserify','webworkerify','uglify']);
+    grunt.registerTask('default', ['browserify','webworkerify','uglify','addLicense']);
 
     grunt.registerTask('webworkerify','Fixes the browserify bundle so it works in Web Workers',function(){
         var src = fs.readFileSync("build/p2.js");
         fs.writeFileSync("build/p2.js",src.toString().replace("global.p2","self.p2"));
+    });
+
+    grunt.registerTask('addLicense','Adds the LICENSE to the top of the built files',function(){
+        var text = fs.readFileSync("LICENSE").toString();
+
+        var dev = fs.readFileSync("build/p2.js").toString();
+        var min = fs.readFileSync("build/p2.min.js").toString();
+
+        fs.writeFileSync("build/p2.js",text+"\n"+dev);
+        fs.writeFileSync("build/p2.min.js",text+"\n"+min);
     });
 };
