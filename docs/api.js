fc0ed900e8652cd706c6c84ccc8d81e18a078d46
Stefan Hedman <schteppe@gmail.com>
2013-09-26 12:33:50 +0200

    updated docs

--- docs/api.js
+++ docs/api.js
@@ -3,6 +3,7 @@ YUI.add("yuidoc-meta", function(Y) {
     "classes": [
         "Body",
         "Broadphase",
+        "Capsule",
         "Circle",
         "Constraint",
         "ContactEquation",
