b00005b9ff21d9f9a9e5c3ad6ddd222adde409b1
Stefan Hedman <schteppe@gmail.com>
2013-10-13 13:07:43 +0200

    added Body test

---
+++ test/Body.js
@@ -0,0 +1,20 @@
+var Body = require('../src/objects/Body')
+,   vec2 = require('../src/math/vec2')
+
+exports.construct = function(test){
+    new Body();
+    test.done();
+};
+
+exports.fromConcave = function(test){
+    var b = new Body();
+    test.ok(b.fromConcavePath( [[-1, 1],
+                                [-1, 0],
+                                [1, 0],
+                                [1, 1],
+                                [0.5, 0.5]]));
+
+    test.ok(b.shapes.length > 0);
+
+    test.done();
+};
