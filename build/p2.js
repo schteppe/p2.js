9b2199eada256a6dd31f4b60b8d3c60f9e813f3b
Stefan Hedman <schteppe@gmail.com>
2013-11-27 08:43:31 +0100

    updated build

--- build/p2.js
+++ build/p2.js
@@ -4213,19 +4213,31 @@ function LockConstraint(bodyA,bodyB,localOffsetB,localAngleB,maxForce){
         maxForce = Number.MAX_VALUE;
 
     localOffsetB = localOffsetB || vec2.fromValues(0,0);
+    localOffsetB = vec2.fromValues(localOffsetB[0],localOffsetB[1]);
+
     localAngleB = localAngleB || 0;
 
     // Use 3 equations:
     // gx =   (xj - xi - l) * xhat = 0
     // gy =   (xj - xi - l) * yhat = 0
-    // ...and one for locking the angle.
+    // gr =   (xi - xj + r) * that = 0
     //
-    // For the position constraints, we get
+    // ...where:
+    //   l is the localOffsetB vector rotated to world in bodyA frame
+    //   r is the same vector but reversed and rotated from bodyB frame
+    //   xhat, yhat are world axis vectors
+    //   that is the tangent of r
+    //
+    // For the first two constraints, we get
     // G*W = (vj - vi - ldot  ) * xhat
     //     = (vj - vi - wi x l) * xhat
     //
     // Since (wi x l) * xhat = (l x xhat) * wi, we get
-    // G*W = [ -1   0   (-l x xhat)  1   0   0]
+    // G*W = [ -1   0   (-l x xhat)  1   0   0] * [vi wi vj wj]
+    //
+    // The last constraint gives
+    // GW = (vi - vj + wj x r) * that
+    //    = [  1   0  -1  (r x t) ]
 
     var x =     new Equation(bodyA,bodyB,-maxForce,maxForce),
         y =     new Equation(bodyA,bodyB,-maxForce,maxForce),
@@ -4244,26 +4256,44 @@ function LockConstraint(bodyA,bodyB,localOffsetB,localAngleB,maxForce){
         vec2.sub(g,bodyB.position,bodyA.position);
         vec2.sub(g,g,l);
         return g[1];
-    }
-
-    rot.G[2] =  1;
-    rot.G[5] = -1;
-    rot.offset = localAngleB;
+    };
+    var r = vec2.create(),
+        t = vec2.create();
+    rot.computeGq = function(){
+        vec2.rotate(r,localOffsetB,bodyB.angle - localAngleB);
+        vec2.scale(r,r,-1);
+        vec2.sub(g,bodyA.position,bodyB.position);
+        vec2.add(g,g,r);
+        vec2.rotate(t,r,-Math.PI/2);
+        vec2.normalize(t,t);
+        return vec2.dot(g,t);
+    };
 
     this.localOffsetB = localOffsetB;
+    this.localAngleB =  localAngleB;
 
     var eqs = this.equations = [ x, y, rot ];
 }
 LockConstraint.prototype = new Constraint();
 
 var l = vec2.create();
+var r = vec2.create();
+var t = vec2.create();
 var xAxis = vec2.fromValues(1,0);
 var yAxis = vec2.fromValues(0,1);
 LockConstraint.prototype.update = function(){
-    var x = this.equations[0],
-        y = this.equations[1];
+    var x =   this.equations[0],
+        y =   this.equations[1],
+        rot = this.equations[2],
+        bodyA = this.bodyA,
+        bodyB = this.bodyB;
 
     vec2.rotate(l,this.localOffsetB,bodyA.angle);
+    vec2.rotate(r,this.localOffsetB,bodyB.angle - this.localAngleB);
+    vec2.scale(r,r,-1);
+
+    vec2.rotate(t,r,Math.PI/2);
+    vec2.normalize(t,t);
 
     x.G[0] = -1;
     x.G[1] =  0;
@@ -4274,6 +4304,12 @@ LockConstraint.prototype.update = function(){
     y.G[1] = -1;
     y.G[2] = -vec2.crossLength(l,yAxis);
     y.G[4] =  1;
+
+    rot.G[0] =  -t[0];
+    rot.G[1] =  -t[1];
+    rot.G[3] =  t[0];
+    rot.G[4] =  t[1];
+    rot.G[5] =  vec2.crossLength(r,t);
 };
 
 },{"../math/vec2":29,"./Constraint":15,"./Equation":18}],21:[function(require,module,exports){
@@ -7402,6 +7438,7 @@ var step_r = vec2.create(),
     step_f = vec2.create(),
     step_fhMinv = vec2.create(),
     step_velodt = vec2.create(),
+    step_mg = vec2.create(),
     xiw = vec2.fromValues(0,0),
     xjw = vec2.fromValues(0,0),
     zero = vec2.fromValues(0,0);
@@ -7431,6 +7468,7 @@ World.prototype.step = function(dt){
         t0, t1,
         fhMinv = step_fhMinv,
         velodt = step_velodt,
+        mg = step_mg,
         scale = vec2.scale,
         add = vec2.add,
         rotate = vec2.rotate;
@@ -7441,13 +7479,15 @@ World.prototype.step = function(dt){
         t0 = now();
     }
 
-    var glen = vec2.length(this.gravity);
+    var glen = vec2.length(g);
 
     // add gravity to bodies
-    if(glen > 0){
+    if(glen !== 0){
         for(var i=0; i!==Nbodies; i++){
-            var fi = bodies[i].force;
-            add(fi,fi,g);
+            var b = bodies[i],
+                fi = b.force;
+            vec2.scale(mg,g,b.mass);
+            add(fi,fi,mg);
         }
     }
 
