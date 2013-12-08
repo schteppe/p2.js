5a5c762d39d54c01edf76d06c640ca151e71aca5
Stefan Hedman <schteppe@gmail.com>
2013-12-08 14:45:12 +0100

    using crossLength in FricitonEquation instead of ugly hack

--- src/constraints/FrictionEquation.js
+++ src/constraints/FrictionEquation.js
@@ -5,19 +5,6 @@ var mat2 = require('../math/mat2')
 
 module.exports = FrictionEquation;
 
-// 3D cross product from glmatrix, until we get this to work...
-function cross(out, a, b) {
-    var ax = a[0], ay = a[1], az = a[2],
-        bx = b[0], by = b[1], bz = b[2];
-
-    out[0] = ay * bz - az * by;
-    out[1] = az * bx - ax * bz;
-    out[2] = ax * by - ay * bx;
-    return out;
-};
-
-var dot = vec2.dot;
-
 /**
  * Constrains the slipping in a contact along a tangent
  *
@@ -95,12 +82,6 @@ FrictionEquation.prototype.setSlipForce = function(slipForce){
     this.minForce = -slipForce;
 };
 
-var A = Utils.ARRAY_TYPE;
-var rixtVec = new A(3),
-    rjxtVec = new A(3),
-    ri3 = new A(3),
-    rj3 = new A(3),
-    t3 = new A(3);
 FrictionEquation.prototype.computeB = function(a,b,h){
     var bi = this.bi,
         bj = this.bj,
@@ -109,26 +90,14 @@ FrictionEquation.prototype.computeB = function(a,b,h){
         t = this.t,
         G = this.G;
 
-    // Caluclate cross products
-    ri3[0] = ri[0];
-    ri3[1] = ri[1];
-    rj3[0] = rj[0];
-    rj3[1] = rj[1];
-    t3[0] = t[0];
-    t3[1] = t[1];
-    cross(rixtVec, ri3, t3);//ri.cross(t,rixt);
-    cross(rjxtVec, rj3, t3);//rj.cross(t,rjxt);
-    var rixt = rixtVec[2];
-    var rjxt = rjxtVec[2];
-
     // G = [-t -rixt t rjxt]
     // And remember, this is a pure velocity constraint, g is always zero!
     G[0] = -t[0];
     G[1] = -t[1];
-    G[2] = -rixt;
+    G[2] = -vec2.crossLength(ri,t);
     G[3] = t[0];
     G[4] = t[1];
-    G[5] = rjxt;
+    G[5] = vec2.crossLength(rj,t);
 
     var GW = this.computeGW();
     var GiMf = this.computeGiMf();
