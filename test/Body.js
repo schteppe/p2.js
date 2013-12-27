9b6fedcd172745769738c4d23ad425fd71a6faf5
Stefan Hedman <schteppe@gmail.com>
2013-12-27 13:12:47 +0100

    added AABB tests for Body

--- test/Body.js
+++ test/Body.js
@@ -1,5 +1,6 @@
 var Body = require('../src/objects/Body')
 ,   vec2 = require('../src/math/vec2')
+,   Circle = require('../src/shapes/Circle')
 
 exports.construct = function(test){
     new Body();
@@ -17,4 +18,41 @@ exports.fromConcave = function(test){
     test.ok(b.shapes.length > 0);
 
     test.done();
+};
+
+exports.updateAABB = {
+    withoutShapes : function(test){
+        var b = new Body();
+        b.updateAABB();
+        test.done();
+    },
+
+    withCircle : function(test){
+        var b = new Body(),
+            s = new Circle(1);
+        b.addShape(s);
+        b.updateAABB();
+
+        test.equal(b.aabb.lowerBound[0], -1, 'Lower AABB bound should be -1');
+        test.equal(b.aabb.upperBound[0],  1, 'Upper AABB bound should be 1');
+        test.equal(b.aabb.lowerBound[1], -1, 'Lower AABB bound should be -1');
+        test.equal(b.aabb.upperBound[1],  1, 'Upper AABB bound should be 1');
+
+        test.done();
+    },
+
+    withOffsetCircle : function(test){
+        var b = new Body(),
+            s = new Circle(1),
+            offset = [-2,3];
+        b.addShape(s,offset,Math.PI/2);
+        b.updateAABB();
+
+        test.equal(b.aabb.lowerBound[0], -s.radius + offset[0]);
+        test.equal(b.aabb.upperBound[0],  s.radius + offset[0]);
+        test.equal(b.aabb.lowerBound[1], -s.radius + offset[1]);
+        test.equal(b.aabb.upperBound[1],  s.radius + offset[1]);
+
+        test.done();
+    },
 };
