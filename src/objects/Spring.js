40e8f4ef90375dc18fc63d46ce2e79e307fb433e
Stefan Hedman <schteppe@gmail.com>
2014-03-30 15:17:01 +0200

    updated Spring docs. cc @Fishrock123

--- src/objects/Spring.js
+++ src/objects/Spring.js
@@ -3,20 +3,20 @@ var vec2 = require('../math/vec2');
 module.exports = Spring;
 
 /**
- * A spring, connecting two bodies.
+ * A spring, connecting two bodies. The Spring explicitly adds force and angularForce to the bodies and does therefore not put load on the solver.
  *
  * @class Spring
  * @constructor
  * @param {Body} bodyA
  * @param {Body} bodyB
  * @param {Object} [options]
- * @param {number} options.restLength   A number > 0. Default: 1
- * @param {number} options.stiffness    A number >= 0. Default: 100
- * @param {number} options.damping      A number >= 0. Default: 1
- * @param {Array}  options.worldAnchorA Where to hook the spring to body A, in world coordinates.
- * @param {Array}  options.worldAnchorB
- * @param {Array}  options.localAnchorA Where to hook the spring to body A, in local body coordinates.
- * @param {Array}  options.localAnchorB
+ * @param {number} [options.restLength=1]   A number > 0. Default: 1
+ * @param {number} [options.stiffness=100]  Spring constant (see Hookes Law). A number >= 0.
+ * @param {number} [options.damping=1]      A number >= 0. Default: 1
+ * @param {Array}  [options.worldAnchorA]   Where to hook the spring to body A, in world coordinates. Overrides the option "localAnchorA" if given.
+ * @param {Array}  [options.worldAnchorB]
+ * @param {Array}  [options.localAnchorA]   Where to hook the spring to body A, in local body coordinates. Defaults to the body center.
+ * @param {Array}  [options.localAnchorB]
  */
 function Spring(bodyA,bodyB,options){
     options = options || {};
