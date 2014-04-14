56193aeeb2c7bb7c48a4b7daf487e5071494169b
Stefan Hedman <schteppe@gmail.com>
2014-04-14 08:25:08 +0200

    making DistanceConstraint anchor point aware - WIP

--- src/constraints/DistanceConstraint.js
+++ src/constraints/DistanceConstraint.js
@@ -29,6 +29,9 @@ function DistanceConstraint(bodyA,bodyB,distance,options){
      */
     this.distance = distance;
 
+    var localAnchorA = this.localAnchorA = vec2.create();
+    var localAnchorB = this.localAnchorB = vec2.create();
+
     var maxForce;
     if(typeof(options.maxForce)==="undefined" ){
         maxForce = Number.MAX_VALUE;
@@ -39,10 +42,41 @@ function DistanceConstraint(bodyA,bodyB,distance,options){
     var normal = new Equation(bodyA,bodyB,-maxForce,maxForce); // Just in the normal direction
     this.equations = [ normal ];
 
+    // g = (xi - xj).dot(n)
+    // dg/dt = (vi - vj).dot(n) = G*W = [n 0 -n 0] * [vi wi vj wj]'
+
+    // ...and if we were to include offset points (TODO for now):
+    // g =
+    //      (xj + rj - xi - ri).dot(n) - distance
+    //
+    // dg/dt =
+    //      (vj + wj x rj - vi - wi x ri).dot(n) =
+    //      { term 2 is near zero } =
+    //      [-n   -ri x n   n   rj x n] * [vi wi vj wj]' =
+    //      G * W
+    //
+    // => G = [-n -rixn n rjxn]
+
     var r = vec2.create();
+    var ri = vec2.create(); // worldAnchorA
+    var rj = vec2.create(); // worldAnchorB
+    var that = this;
     normal.computeGq = function(){
+        var bodyA = this.bodyA,
+            bodyB = this.bodyB,
+            xi = bodyA.position,
+            xj = bodyB.position;
+
+        // Transform local anchors to world
+        vec2.rotate(ri, localAnchorA, bodyA.angle);
+        vec2.rotate(rj, localAnchorB, bodyB.angle);
+
+        vec2.add(r, xi, rj);
+        vec2.sub(r, r, ri);
+        vec2.sub(r, r, xi);
+
         vec2.sub(r, bodyB.position, bodyA.position);
-        return vec2.length(r)-distance;
+        return vec2.length(r) - that.distance;
     };
 
     // Make the contact constraint bilateral
@@ -55,12 +89,34 @@ DistanceConstraint.prototype = new Constraint();
  * @method update
  */
 var n = vec2.create();
+var ri = vec2.create(); // worldAnchorA
+var rj = vec2.create(); // worldAnchorB
 DistanceConstraint.prototype.update = function(){
     var normal = this.equations[0],
         bodyA = this.bodyA,
         bodyB = this.bodyB,
         distance = this.distance,
+        xi = bodyA.position,
+        xj = bodyB.position,
         G = normal.G;
+
+    // Transform local anchors to world
+    vec2.rotate(ri, this.localAnchorA, bodyA.angle);
+    vec2.rotate(rj, this.localAnchorB, bodyB.angle);
+
+    // Caluclate cross products
+    var rixn = vec2.crossLength(ri, n),
+        rjxn = vec2.crossLength(rj, n);
+
+    /*
+    // G = [-n -rixn n rjxn]
+    G[0] = -n[0];
+    G[1] = -n[1];
+    G[2] = -rixn;
+    G[3] = n[0];
+    G[4] = n[1];
+    G[5] = rjxn;
+    */
 
     vec2.sub(n, bodyB.position, bodyA.position);
     vec2.normalize(n,n);
