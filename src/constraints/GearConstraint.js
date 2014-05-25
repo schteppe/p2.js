7795d04f6d24009d008979f9508597bdb9035b27
Stefan Hedman <schteppe@gmail.com>
2014-05-25 21:26:15 +0200

    setting the offset angle to the current in GearConstraint. Will make it easier to set up. Part of #86

--- src/constraints/GearConstraint.js
+++ src/constraints/GearConstraint.js
@@ -1,7 +1,7 @@
 var Constraint = require('./Constraint')
 ,   Equation = require('../equations/Equation')
 ,   AngleLockEquation = require('../equations/AngleLockEquation')
-,   vec2 = require('../math/vec2')
+,   vec2 = require('../math/vec2');
 
 module.exports = GearConstraint;
 
@@ -13,7 +13,7 @@ module.exports = GearConstraint;
  * @param {Body}            bodyA
  * @param {Body}            bodyB
  * @param {Object}          [options]
- * @param {Number}          [options.angle=0] Relative angle between the bodies.
+ * @param {Number}          [options.angle=0] Relative angle between the bodies. Will be set to the current angle between the bodies (the gear ratio is accounted for).
  * @param {Number}          [options.ratio=1] Gear ratio.
  * @param {Number}          [options.maxTorque] Maximum torque to apply.
  * @extends Constraint
@@ -24,23 +24,27 @@ function GearConstraint(bodyA, bodyB, options){
 
     Constraint.call(this, bodyA, bodyB, Constraint.GEAR, options);
 
-    this.equations = [
-        new AngleLockEquation(bodyA,bodyB,options),
-    ];
-
-    /**
-     * The relative angle
-     * @property angle
-     * @type {Number}
-     */
-    this.angle = typeof(options.angle) === "number" ? options.angle : 0;
-
     /**
      * The gear ratio.
      * @property ratio
      * @type {Number}
      */
     this.ratio = typeof(options.ratio) === "number" ? options.ratio : 1;
+
+    /**
+     * The relative angle
+     * @property angle
+     * @type {Number}
+     */
+    this.angle = typeof(options.angle) === "number" ? options.angle : bodyB.angle - this.ratio * bodyA.angle;
+
+    // Send same parameters to the equation
+    options.angle = this.angle;
+    options.ratio = this.ratio;
+
+    this.equations = [
+        new AngleLockEquation(bodyA,bodyB,options),
+    ];
 
     // Set max torque
     if(typeof(options.maxTorque) === "number"){
