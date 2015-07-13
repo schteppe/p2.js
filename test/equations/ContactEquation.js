1d19827bb6a8997d880bd906c19f488588ed07df
Stefan Hedman <schteppe@gmail.com>
2015-07-13 17:51:08 +0200

    added ContactEquation.prototype.getVelocityAlongNormal /cc GoodBoyDigital

--- test/equations/ContactEquation.js
+++ test/equations/ContactEquation.js
@@ -13,3 +13,16 @@ exports.computeB = function(test){
     // TODO
     test.done();
 };
+
+exports.getVelocityAlongNormal = function(test){
+    var bodyA = new Body({ position: [-1, 0] });
+	var bodyB = new Body({ position: [1, 0] });
+    var eq = new ContactEquation(bodyA, bodyB);
+	test.equal(eq.getVelocityAlongNormal(), 0);
+	eq.normalA[0] = 1; // points out from bodyA toward bodyB
+	eq.normalA[1] = 0;
+	bodyB.velocity[0] = 1; // Away from bodyA along the positive normal direction
+	test.equal(eq.getVelocityAlongNormal(), -1);
+
+    test.done();
+};
