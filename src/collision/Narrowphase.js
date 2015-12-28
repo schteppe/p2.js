2f83e47f878131f104604fbbd4e8d6604684e974
Stefan Hedman <schteppe@gmail.com>
2015-12-28 09:56:54 +0100

    optimized pointInConvex in Narrowphase

--- src/collision/Narrowphase.js
+++ src/collision/Narrowphase.js
@@ -1137,8 +1137,7 @@ Narrowphase.prototype.circleConvex = function(
     return 0;
 };
 
-var pic_worldVertex0 = vec2.create(),
-    pic_worldVertex1 = vec2.create(),
+var pic_localPoint = vec2.create(),
     pic_r0 = vec2.create(),
     pic_r1 = vec2.create();
 
@@ -1146,29 +1145,24 @@ var pic_worldVertex0 = vec2.create(),
  * Check if a point is in a polygon
  */
 function pointInConvex(worldPoint,convexShape,convexOffset,convexAngle){
-    var worldVertex0 = pic_worldVertex0,
-        worldVertex1 = pic_worldVertex1,
+    var localPoint = pic_localPoint,
         r0 = pic_r0,
         r1 = pic_r1,
-        point = worldPoint,
         verts = convexShape.vertices,
         lastCross = null;
+
+    vec2.toLocalFrame(localPoint, worldPoint, convexOffset, convexAngle);
+
     for(var i=0; i!==verts.length+1; i++){
         var v0 = verts[i%verts.length],
             v1 = verts[(i+1)%verts.length];
 
-        // Transform vertices to world
-        // @todo The point should be transformed to local coordinates in the convex, no need to transform each vertex
-        vec2.rotate(worldVertex0, v0, convexAngle);
-        vec2.rotate(worldVertex1, v1, convexAngle);
-        add(worldVertex0, worldVertex0, convexOffset);
-        add(worldVertex1, worldVertex1, convexOffset);
+        sub(r0, v0, localPoint);
+        sub(r1, v1, localPoint);
 
-        sub(r0, worldVertex0, point);
-        sub(r1, worldVertex1, point);
         var cross = vec2.crossLength(r0,r1);
 
-        if(lastCross===null){
+        if(lastCross === null){
             lastCross = cross;
         }
 
